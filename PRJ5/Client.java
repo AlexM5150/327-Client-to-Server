c4387a405f7903cd3d1bb63db356441dce89a6c0
Alex M <33159385+AlexM5150@users.noreply.github.com>
2022-11-15 21:02:37 -0800

    comments edit

--- PRJ5/Client.java
+++ PRJ5/Client.java
@@ -5,36 +5,40 @@ import java.util.*;
 public class Client
 {
     public static void main(String[] args) {
+        //define initial variables
         String strIp, strMessage;
         int port;
         DatagramSocket sock = null;
         Scanner sc = new Scanner(System.in);
+        Console console = System.console();
 
         try {
-            Console console = System.console();
+            //get ip and port via user input
             System.out.println("Enter IP address: ");
             strIp = sc.nextLine();
+            //convert string ip into InetAddress object
             InetAddress ip = InetAddress.getByName(strIp);
-
             System.out.println("Enter Port: ");
             port = sc.nextInt();
+            //infinite loop to send unlimited messages
             while (true) {
-                
+                //create new datagram socket and set 15 sec timeout so a socket isn't left hanging with no reply
                 sock = new DatagramSocket();
-                //sock.setSoTimeout(5000);
-
-                //System.out.println("Enter Message: ");
-                strMessage = console.readLine("\nMessage: ");//sc.nextLine();
+                sock.setSoTimeout(15000);
+                //console application to read the message from user input
+                strMessage = console.readLine("\nMessage: ");
+                //convert string message into bytes
                 byte [] message = strMessage.getBytes();
-
+                //put message into a packet with ip/port to send to server and packet is sent
                 DatagramPacket request = new DatagramPacket(message, message.length, ip, port);
                 sock.send(request);
-                
+                //byte size is set for reply from server and reply packet is recieved
                 byte[] buffer = new byte[1000];
                 DatagramPacket reply = new DatagramPacket(buffer, buffer.length);
                 sock.receive(reply);
                 System.out.println("Reply: " + new String(reply.getData()));
             }
+        //exceptions to catch socket execptions and user input errors    
         }catch (SocketException e){System.out.println("Socket: " + e.getMessage());
         }catch (SocketTimeoutException s){System.out.println("Socket timed out, check the port");
         }catch (IOException e) {System.out.println("IO: " + e.getMessage());}
--- PRJ5/Server.java
+++ PRJ5/Server.java
@@ -5,18 +5,22 @@ import java.io.*;
 public class Server
 {
     public static void main(String[] args) {
+        //define initial variables
     	String upper;
         DatagramSocket sock = null;
         int port;
         Scanner sc = new Scanner(System.in);
         
     	try {
+            //get port from user input
             System.out.println("Enter Port: ");
             port = sc.nextInt();
+            //create new socket on that port
             sock = new DatagramSocket(port);
             System.out.println("Listening for connections on port " + port +" ...");
-        
+            //infinite loop so unlimited messages can be recieved and replies sent
             while (true) {
+                //byte size set and is reset each iteration 
                 byte[] buffer = new byte[1000];
             	DatagramPacket request = new DatagramPacket(buffer, buffer.length);
                 sock.receive(request);
@@ -26,10 +30,11 @@ public class Server
                 upper = upper.toUpperCase();
                 System.out.println("Message Sent: " + upper);
                 byte [] b = upper.getBytes();
+                //reply packet is created with new uppercase message and sent to client
                 DatagramPacket reply = new DatagramPacket(b, b.length, request.getAddress(), request.getPort());
                 sock.send(reply);
             }
-            
+        //exceptions to catch socket execptions and user input errors                
         }catch (SocketException e){System.out.println("Socket: " + e.getMessage());
         }catch (IOException e) {System.out.println("IO: " + e.getMessage());}
      {if(sock != null) sock.close();}
